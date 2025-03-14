bad2c65f5b3ff814ed46ec3dbff86e09d0722912
Luis David Florez Pareja <luisflorexpareja@gmail.com>
2025-03-13 21:28:09 -0500

    Add sample service

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+
+
+app = FastAPI(title="Test API", description="This is a test API", version="0.0.1")
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/items")
+def create_item(item: Item):
+    return item
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
